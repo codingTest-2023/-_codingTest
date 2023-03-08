06312eefdb3045734680b63680f5b92245f30e51
chesthyeon <niw927@naver.com>
2023-03-09 01:09:27 +0900

    프로그래머스 대충만든 자판

---
+++ src/_0309_02.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+//프로그래머스 대충만든 자판
+public class _0309_02 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new String[]{"ABACD", "BCEFD"}, new String[]{"ABCD","AABB"}));
+    }
+    static class Solution {
+        public List<Integer> solution(String[] keymap, String[] targets) {
+            HashMap<Character, Integer> hashMap = new HashMap<>();
+            List<Integer> answer = new ArrayList<>();
+            for (int i = 0; i < targets.length; i++){
+                answer.add(0);
+            }
+            for (String key : keymap){
+                for (int i = 0; i < key.length(); i++){
+                    char alpha = key.charAt(i);
+                    hashMap.put(alpha, Math.min(hashMap.getOrDefault(alpha, i + 1),i + 1));
+                }
+            }
+            for (int i = 0; i < targets.length; i++){
+                for (int j = 0; j < targets[i].length(); j++){
+                    char alpha = targets[i].charAt(j);
+                    if (hashMap.containsKey(alpha)) answer.set(i,answer.get(i) + hashMap.get(alpha));
+                    else {
+                        answer.set(i, -1);
+                        break;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+}
