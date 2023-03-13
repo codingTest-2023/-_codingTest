05074d6c98955b7d64b03e46376f56aa03fce590
chesthyeon <niw927@naver.com>
2023-03-13 21:40:00 +0900

    프로그래머스 문자열 나누기 lv1

---
+++ src/_0313_02.java
@@ -0,0 +1,27 @@
+import java.util.*;
+// 프로그래머스 디펜스 게임 lv 2
+public class _0313_02 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(	7, 3, new int[]{4, 2, 4, 5, 3, 3, 1}));
+    }
+    static class Solution {
+        public int solution(int n, int k, int[] enemy) {
+            int answer = 0;
+            if (k >= enemy.length) return enemy.length;
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> o2 - o1);
+            for (int e : enemy){
+                n -= e;
+                answer += 1;
+                priorityQueue.add(e);
+                if (n < 0){
+                    if (k == 0){
+                        return answer -= 1;
+                    }
+                    n = n + priorityQueue.poll();
+                    k--;
+                }
+            }
+            return answer;
+        }
+    }
+}
